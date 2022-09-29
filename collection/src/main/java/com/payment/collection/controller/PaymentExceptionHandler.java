1e1f9422042b6c7b7940e23cc02daf9cf4443032
Laurel Lao Ang <laurellaoang@gmail.com>
2022-09-29 15:32:05 +0800

    setup controller and service

---
+++ collection/src/main/java/com/payment/collection/controller/PaymentExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.payment.collection.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class PaymentExceptionHandler {
+}
---
+++ collection/src/main/java/com/payment/collection/service/PaymentService.java
@@ -0,0 +1,5 @@
+package com.payment.collection.service;
+
+public interface PaymentService {
+    void displayAllData();
+}
