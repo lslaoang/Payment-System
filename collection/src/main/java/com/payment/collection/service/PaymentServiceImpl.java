ead2d5662d1b0bdd5a673e5cdec88b7ac5d22127
Laurel Lao Ang <laurellaoang@gmail.com>
2022-09-30 18:28:00 +0800

    update service

--- collection/src/main/java/com/payment/collection/service/PaymentServiceImpl.java
+++ collection/src/main/java/com/payment/collection/service/PaymentServiceImpl.java
@@ -1,15 +1,22 @@
 package com.payment.collection.service;
 
 import com.payment.collection.model.Transaction;
+import com.payment.collection.repo.PaymentRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
     @Override
     public List<Transaction> displayAllData() {
-        return Collections.emptyList();
+        return paymentRepository.findAll();
     }
 }
